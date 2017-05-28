1a641c61f957125d52fdb22933b2b526bc625be9
Larry <larryosak@gmail.com>
2017-05-27 23:01:51 -0700

    Add toast message to display score

--- app/src/main/java/com/example/larryosakwe/quizapp/MainActivity.java
+++ app/src/main/java/com/example/larryosakwe/quizapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.larryosakwe.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int correctPoints = 0; //number of correct answers
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+    /*
+    When the submit button is clicked, the user's score is counted and shows up as a Toast message
+     */
+    public void submitQuiz(View view) {
+        Toast toastScore = Toast.makeText(getApplicationContext(), "You scored " + correctPoints + " points!", Toast.LENGTH_SHORT);
+        toastScore.show();
     }
 }
